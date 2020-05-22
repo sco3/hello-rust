9e4afd93035e3a5fa0c608a10722ccf100334e82
scorta <scorta@gmail.com>
2020-05-22 17:32:16 +0100

    remove old code

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ for (var i = 0; i < n; i++) {
 			str += '.';
 		} else {
 			seed = (((seed + 17) * 13) / 11) & 0xffff;
-			// console.log("seed %d = %d", i + j, seed);
-
 			str += String.fromCharCode(0x30 + (seed % 10));
 		}
 	}
