69c5a8f5f67ff4467e898e034b7f1f1cf3be34ae
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-01-10 17:03:49 +0000

    julia implementation

---
+++ src/main.jl
@@ -0,0 +1,26 @@
+#!/usr/bin/env julia
+
+
+using Printf
+
+function run()
+   str = collect("10000000.00000007")
+   seed = Int32(1)
+   num = Float64(0)
+   n = 10000000
+   fstr = ""
+   for i in 1:n
+    for j in 1:17
+      if j!=9
+         seed = div(((seed + 7) << 4), 11) & 0xffffff
+         str[j] = '0' + (seed % 10)
+     
+      end
+    end
+    fstr = join(str)
+    num = parse(Float32, fstr)
+   end
+   @printf "Random numbers parsed: %d str: %s num: %17.8f\n" n fstr num
+end
+
+@time run()
