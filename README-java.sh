aaec41f139c141bed83a0e4c58cf90bac4ffcb21
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-04-21 21:59:15 +0100

    java fast

--- README-java.sh
+++ README-java.sh
@@ -22,6 +22,19 @@ $(~/prg/graalvm/bin/java -version 2>&1| tail -n 1)
 
 $(~/prg/graalvm/bin/java -cp target Main)
 
+
+Fast double parser:
+
+$(bash -x -c '~/prg/graalvm/bin/javac  -cp "lib/*" -d target src/MainFast.java ' 2>&1)
+
+$(~/prg/graalvm/bin/java -version 2>&1| tail -n 1)
+
+$(~/prg/graalvm/bin/java -cp "lib/*:target" MainFast)
+
+
+
+
+
 $(bash -x -c '~/prg/graalvm/bin/native-image -O3 --no-fallback -cp target Main target/Main > /dev/null' 2>&1)
 
 $(target/Main)
