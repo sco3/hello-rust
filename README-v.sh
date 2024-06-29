2b16541af6302aa36fc869449973b93430a8ff65
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-06-29 16:40:55 +0100

    v

--- README-v.sh
+++ README-v.sh
@@ -10,14 +10,14 @@ V Lang:
 ~~~
 Unsafe: (byte array with tos() to string conversion)
 
-$(bash -x -c 'v -prod src/main.v  -o target/vmain' 2>&1  )
+$(bash -x -c 'v -prod src/main/v/main.v  -o target/vmain' 2>&1  )
 
 $( target/vmain )
 
 With strings.Builder
 
-$(bash -x -c 'v -prod -autofree src/main-builder.v  -o target/vmain' 2>&1  )
+$(bash -x -c 'v -prod -autofree src/main/v/main-builder.v  -o target/vmainb' 2>&1  )
 
-$( target/vmain )
+$( target/vmainb )
 ~~~
 EOF
