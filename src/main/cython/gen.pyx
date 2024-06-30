187cde53c727bb52649ed2c1571204bf2ae77ef2
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-06-30 22:22:49 +0100

    somehow works

--- src/main/cython/gen.pyx
+++ src/main/cython/gen.pyx
@@ -1,8 +1,10 @@
 
 import time
 
+from libc.stdlib cimport malloc, free
+from libc.stdio cimport printf
 
-def gen(int seed , char *slist):
+cdef gen(int seed , char *slist):
     """
     Generates pseudo random number
     """
@@ -10,7 +12,12 @@ def gen(int seed , char *slist):
     for j in range(17):
         if j != 8:
             seed = (((seed + 7) << 4) // 11) & 0xFFFFFF
-            #slist[j] = 0x30 + (seed % 10)
+            slist[j] = 0x30 + (seed % 10)
+        else:
+            slist[j] = ord('.')
+            
+    printf("Pointer address: %p\n", <void*>slist)
+    print ("in gen: ", slist[:17].decode())
     return seed
 
 
@@ -21,14 +28,17 @@ def main():
 
     start = time.time_ns()
     cdef double num = 0.0
-    cdef char *slist[18]
-    slist[17]='\0'
+    cdef char *slist = <char *>malloc(18)
+    slist[17] = 0
     cdef int n = 10 # 10000000
     cdef int seed = 1
     for _ in range(n):
         seed = gen(seed, slist)
-        print (f"{slist}")
-        num = float(slist)
+        printf("Pointer address: %p\n", <void*>slist)
+        #s = slist[:17].decode('utf-8')
+        #print (f"in main: {s}")
+        #num = float (s)
+
     print(f"Random numbers parsed: {n} str: {slist} num: {num}")
     duration = int((time.time_ns()-start)/1000000)
     print(f"Time: {duration} ms")
