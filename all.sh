fa387b9c041b23e93573992da7f110f6082786fd
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-08-08 16:29:18 +0100

    update toml and make it work, almos all except mojo

--- all.sh
+++ all.sh
@@ -1,9 +1,12 @@
 
 
+rm -rf target
+mkdir -p target
+
 rm -rf README$(./cpu.sh).md
 
 cat /proc/cpuinfo | grep 'model name' | sort -u | awk '{ $1=""; $2="";$3="" ;print }'  > README$(./cpu.sh).md
 echo "===" >> README$(./cpu.sh).md
 mkdir -p ~/tmp
-ls README*.sh | sort | awk '{ print "./" $1 }'  > ~/tmp/all.sh
+ls README*.sh | grep -v mojo | sort | awk '{ print "./" $1 }'  > ~/tmp/all.sh
 bash ~/tmp/all.sh
--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S bash
 
-echo "-$(cat /proc/cpuinfo | grep 'model name' | sort -u | grep -ohE 'i[0-9]-[0-9]{4}[A-Za-z]')"
+echo "-$(cat /proc/cpuinfo | grep 'model name' | sort -u | grep -oE 'i[0-9]-.*$' )"
